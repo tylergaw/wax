92b33f05fe4343b0bb9312555a52cad2d8d494ad
Tyler Gaw <me@tylergaw.com>
2023-01-18 10:11:34 -0500

    Removes dead file

--- scripts/transformCollection.js
+++
@@ -1,7 +0,0 @@
-import collection from "../src/data/collection.json" assert { type: "json" };
-
-const descriptions = collection.map((r) => {
-  return r.basic_information.formats[0].descriptions;
-});
-
-console.log(descriptions);

acf9a32b892fb7651e03706081bb2a6e8b798af0
Tyler Gaw <me@tylergaw.com>
2024-08-18 03:33:22 -0400

    Adds SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -6,4 +7,6 @@ export default defineConfig({
   // NOTE: This will be the default in 3.x
   // See https://github.com/withastro/astro/releases/tag/astro%402.4.0
   scopedStyleStrategy: "class",
+  output: "hybrid",
+  adapter: netlify(),
 });
